5a8995387399e86f67e0b900ff78d6c197534401
VADIM <nagiozv2@gmail.com>
2021-08-31 13:47:24 +0400

    fix 4rd task

--- Задание 4/script.js
+++ Задание 4/script.js
@@ -4,9 +4,7 @@ const btn = document.querySelector(".btn");
 const result = document.querySelector(".result");
 
 btn.addEventListener("click", () => {
-  if (imgWidth.value > 300 || imgWidth.value < 100) {
-    result.innerText = "Одно из чисел не попадает в диапозон от 100 до 300";
-  } else if (imgHeight.value > 300 || imgHeight.value < 100) {
+  if ((imgWidth.value > 300 || imgWidth.value < 100 ) || (imgHeight.value > 300 || imgHeight.value < 100)) {
     result.innerText = "Одно из чисел не попадает в диапозон от 100 до 300";
   } else {
     fetch(`https://picsum.photos/${imgWidth.value}/${imgHeight.value}`)
